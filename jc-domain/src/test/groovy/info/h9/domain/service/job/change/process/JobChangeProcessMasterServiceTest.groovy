2aa5a44cea3a0139750d3cea5694d3254efc7363
hangyuCho <chg3376@gmail.com>
2023-10-09 10:44:42 +0900

    fix : test properties setting

---
+++ jc-domain/src/test/groovy/info/h9/domain/service/job/change/process/JobChangeProcessMasterServiceTest.groovy
@@ -0,0 +1,21 @@
+package info.h9.domain.service.job.change.process
+
+import info.h9.domain.BaseTest
+import org.springframework.beans.factory.annotation.Autowired
+
+class JobChangeProcessMasterServiceTest extends BaseTest {
+
+    @Autowired
+    JobChangeProcessMasterService jobChangeProcessMasterService
+
+    def "getJobChangeProcessMasterList"() {
+        given:
+        buildTestDate("setup/service/event/event.sql")
+
+        when:
+        def result = jobChangeProcessMasterService.getJobChangeProcessMasterList()
+
+        then:
+        result != null
+    }
+}
