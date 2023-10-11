503c7d2a11ececb71f16c5e99d65dcc23667ae9a
hangyuCho <chg3376@gmail.com>
2023-10-12 01:23:06 +0900

    fix : refactoring for project structure

---
+++ jc-domain/src/main/java/info/h9/domain/DomainConfig.java
@@ -0,0 +1,7 @@
+package info.h9.domain;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication(scanBasePackageClasses = DomainApplication.class)
+public class DomainConfig {
+}
